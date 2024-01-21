c9f557256c1aae4f96fab13b789fafc22052e9a3
Shyan W. <shyanw321@gmail.com>
2024-01-20 22:37:09 -0500

    Google Map Showing with Search

---
+++ src/components/GoogleMaps.jsx
@@ -0,0 +1,67 @@
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import './GoogleMaps.css';
+import { useRef, useCallback } from "react";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "80vw",
+  height: "80vh",
+};
+
+const center = {
+  lat: 40.7128,
+  lng: 74.0060,
+};
+
+const GoogleMaps = () => {
+  const autocompleteInputRef = useRef(null);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const initAutocomplete = useCallback((map) => {
+    if (!autocompleteInputRef.current) return;
+
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      autocompleteInputRef.current
+    );
+    autocomplete.setFields(['place_id', 'geometry', 'name']);
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+    });
+  }, []);
+
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading maps</div>;
+
+  return (
+    <>
+      <input ref={autocompleteInputRef} type="text" placeholder="Where to?" />
+      <div className='google-maps'>
+        <GoogleMap
+           mapContainerStyle={ mapContainerStyle}
+          center={center}
+          zoom={8}
+          onLoad={initAutocomplete}
+        />
+    
+       
+      </div>
+    </>
+  );
+};
+
+export default GoogleMaps;
