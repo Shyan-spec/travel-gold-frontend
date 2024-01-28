ac01c4bbbb98ecd7dc575c51052aa072a21f2319
Shyan W. <shyanw321@gmail.com>
2024-01-28 17:01:58 -0500

    Final Draft 1.0

--- src/pages/ ItineraryDetailsPage/ItineraryDetailsPage.jsx
+++ src/pages/ ItineraryDetailsPage/ItineraryDetailsPage.jsx
@@ -5,15 +5,45 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 import styles from "./ItineraryDetailsPage.module.css";
-import { useState, useCallback, useRef } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
+import { useLocation, useParams } from "react-router-dom";
+import axios from "axios";
 
 const ItineraryDetailsPage = () => {
   const libraries = ["places"];
+  const [placesOfInterest, setPlacesOfInterest] = useState([]);
   const autocompleteInputRef = useRef(null);
+
+
+  const { itinerary } = useParams();
+  const [itineraryDetails, setItineraryDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchItinerary = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACK_END_SERVER_URL}/itineraries/${itinerary}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`, // Assuming a Bearer token, adjust if different
+              },
+        });
+        setItineraryDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching itinerary:', error);
+      }
+    };
+
+    if (!itineraryDetails) {
+      fetchItinerary();
+    }
+  }, [itinerary, itineraryDetails]);
+
+  console.log(itinerary,itineraryDetails)
+
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
     libraries,
@@ -41,7 +71,6 @@ const ItineraryDetailsPage = () => {
           return;
         }
 
-
         if (place.geometry.viewport) {
           map.fitBounds(place.geometry.viewport);
         } else {
@@ -55,33 +84,32 @@ const ItineraryDetailsPage = () => {
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className={styles.detailsContainer}>
-      <div className={styles.detailsHeader}></div>
+        <div className={styles.detailsHeader}></div>
         <div className={styles.detailsMap}>
-        {isLoaded && (
-              <GoogleMap
-                mapContainerStyle={{
-                  width: "95%",
-                  height: "95%",
-                  borderRadius: "10px",
-                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-                }}
-                center={center}
-                zoom={13}
-                onLoad={initAutocomplete}
-                
-              ></GoogleMap>
-            )}
+          {isLoaded && (
+            <GoogleMap
+              mapContainerStyle={{
+                width: "95%",
+                height: "95%",
+                borderRadius: "10px",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+              }}
+              center={center}
+              zoom={13}
+              onLoad={initAutocomplete}
+            ></GoogleMap>
+          )}
         </div>
         <div className={styles.detailsInfo}>
-        <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography variant="h1" sx={{ fontSize: "2rem" }}>
-          Itinerary:
-        </Typography>
-        <Typography variant="body2">
-          {/* {pointsOfInterest.map((location, index) => {
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h1" sx={{ fontSize: "2rem" }}>
+                Itinerary:
+              </Typography>
+              <Typography variant="body2">
+                {/* {pointsOfInterest.map((location, index) => {
             return (
               <>
                 <div key={index}>
@@ -91,9 +119,9 @@ const ItineraryDetailsPage = () => {
               </>
             );
           })} */}
-        </Typography>
-      </CardContent>
-    </Card>
+              </Typography>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </>
