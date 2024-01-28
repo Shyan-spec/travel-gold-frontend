091bfd41440c4afea53cc2066e9fd8d739f4229f
Aldiana <hotaldiana@gmail.com>
2024-01-28 12:39:04 -0500

    front end changes

--- src/pages/MyItinerariesPage/MyItinerariesPage.jsx
+++ src/pages/MyItinerariesPage/MyItinerariesPage.jsx
@@ -1,10 +1,11 @@
-
-
 import React from 'react';
-
+import './MyitinerariesPage.css'
+import {Navbar} from '../../components/Navbar/Navbar'
 const MyItinerariesPage = () => {
   return (
     <div>
+      <Navbar />
+
       <h2>This is My Itineraries Page</h2>
       {/* Add your content for the My Itineraries page */}
     </div>
